b02739157cafd92a9048cc0caf0c8dc234b66fd1
Masahiko G. Yamada <myspinor@gmail.com>
2026-04-25 21:03:08 +0900

    Add canonical fusion-rule stable keys for grouping

--- src/ACMG.jl
+++ src/ACMG.jl
@@ -108,7 +108,7 @@ export find_mtcs_at_prime, signed_Fp
 export acmg_crt, crt2, rational_reconstruct, compute_sqrt_d_mod_p
 export compute_sqrt3_cyclotomic_mod_p, compute_sqrt2_cyclotomic_mod_p
 export compute_sqrt5_cyclotomic_mod_p
-export fusion_signature, group_mtcs_by_fusion, group_mtcs_galois_aware
+export fusion_signature, canonical_rule, group_mtcs_by_fusion, group_mtcs_galois_aware
 export reconstruct_rational, reconstruct_in_Z_sqrt_d
 export reconstruct_matrix_in_Z_sqrt_d, reconstruct_S_matrix
 export verify_reconstruction, describe_matrix
